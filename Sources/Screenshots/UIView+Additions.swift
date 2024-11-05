a5ea83e3cd5daede563f002c52286db20c8e6f62
Rizwan Shah <rix.jafri@gmail.com>
2024-11-05 14:58:45 +0500

    invalid size crash fixed
    invalid size crash fixed

--- Sources/Screenshots/UIView+Additions.swift
+++ Sources/Screenshots/UIView+Additions.swift
@@ -6,6 +6,12 @@ extension UIView {
     /// - Parameter croppingRect: a rect to focus the screenshot
     /// - Returns: an `UIImage` with the visible contents inside the cropping rect
     func screenshotForCroppingRect(croppingRect: CGRect) -> UIImage? {
+        // Ensure that the croppingRect has valid dimensions
+        guard croppingRect.size.width > 0, croppingRect.size.height > 0 else {
+            print("Invalid croppingRect size: \(croppingRect.size)")
+            return nil
+        }
+
         UIGraphicsBeginImageContextWithOptions(croppingRect.size, false, UIScreen.main.scale)
 
         guard let context = UIGraphicsGetCurrentContext() else {
